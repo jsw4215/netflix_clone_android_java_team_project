8f3a0b7d5d5d2d4297579e65b6a95ec2662a8678
jsw4215 <jsw4215van@gamil.com>
2020-06-28 22:52:08 +0900

    netfilx splash and profile selsetion

---
+++ app/src/main/java/com/daniel/app/netfilx_clone/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.daniel.app.netfilx_clone.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
