3ccc86a2371792cfb3f1fddd0a06ca2bfc6b65f9
jsw4215 <jsw4215van@gamil.com>
2020-06-30 01:55:28 +0900

    bottomnav & else ing

---
+++ app/src/main/java/com/daniel/app/netfilx_clone/ui/more/MoreActivity.java
@@ -0,0 +1,14 @@
+package com.daniel.app.netfilx_clone.ui.more;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+    }
+}
